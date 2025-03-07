6e8664a1d2c52f1367e1cebd620a6358e9031c25
lerchenflo <florian.lerchenmueller@gmail.com>
2025-03-07 10:37:00 +0100

    Constants klasse

---
+++ app/src/main/java/com/lerchenflo/t10elementekatalog/constants.java
@@ -0,0 +1,110 @@
+package com.lerchenflo.t10elementekatalog;
+
+public class constants {
+    public static final String[] Boden = {
+            "Beliebige Rolle vorwärts",
+            "Beliebige Rolle rückwärts ODER Kerze 2 Sek.",
+            "Krafthandstand ODER Sprung mit Querspreizwinkel 135°",
+            "C+Position in Rückenlage 2 Sek. ODER Kopfstand 2 Sek ODER 1/1 Dr. vw. auf gestreckem Bein",
+            "VERBINDUNG Chassé – Pferdchen-/Schersprung ODER Sprung mit 1/1 Dr.",
+            "Beliebige Standwaage 2 Sek. ODER Spagat 2 Sek. ODER Winkelstütz 2 Sek",
+            "Handstand ODER Handstand mit mind. ½ Dr.",
+            "Rad ODER einarmiges Rad",
+            "Rondat (Radwende)",
+            "Handstützüberschlag vw. ODER Handstützüberschlag rw. (Flick-Flack)",
+            "Salto vorwärts geh. ODER Salto rückwärts geh."
+    };
+
+    public static final String[] Barren = {
+            "Sprung in den Stütz und Vorschwung ODER beliebige (andere) Schwungstemme",
+            "4 Schwünge",
+            "Vorschwung in den Grätschsitz ODER Oberarmkippe in den Grätschsitz",
+            "Außenquersitz",
+            "Winkelstütz 2 Sek.",
+            "Rolle vw. ODER Rolle rw.",
+            "Oberarmstand 2 Sek.",
+            "Beliebige Kippe in den Stütz",
+            "Beliebige (andere) Schwungstemme ODER Rückschwung in den Handstand",
+            "Abgang: Kehre ODER Wende ODER Kreishocke"
+    };
+
+    public static final String[] Ringe = {
+            "Stütz 2 Sek.",
+            "Winkelstütz 2 Sek. ODER Grätschwinkelstütz 2 Sek.",
+            "Kontrolliertes Absenken ODER Abrollen aus dem Stütz",
+            "Beugehang 2 Sek.",
+            "Winkelhang 2 Sek. mit gehockten ODER gestreckten Beinen",
+            "Strecksturzhang 2 Sek.",
+            "Aus (Streck-)Sturzhang senken zum Hang(stand) rl.",
+            "Heben vom Hang rl. zum Sturzhang",
+            "4 Schwünge",
+            "Abgang: Salto vorwärts ODER Salto rückwärts"
+    };
+
+
+
+    public static final String[] Tiefreck = {
+            "Hüftaufschwung",
+            "Sprung in den Stütz vl. ODER Vor- oder Rückspreizen eines Beines",
+            "Rück- und Vordrehen im Hang ODER Spreizumschwung (Mühle) ODER Sitzumschwung",
+            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
+            "Kipphang 2 Sek. ODER Vorschweben-Rückschweben ODER Vorschweben zum Kästchen 'Beliebige Kippe'",
+            "Beliebige Kippe",
+            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
+            "Hüftumschwung vorlings rückwärts",
+            "VERBINDUNG Umschwung - Unterschwung",
+            "Abgang: Beliebiger Unterschwung ODER Hocke/Hockwende"
+    };
+
+    public static final String[] Hochreck = {
+            "Hüftaufschwung ODER Schwungaufschwung ODER 2. Kippe ODER Schwungstemme rw.",
+            "Vor- oder Rückspreizen eines Beines ODER Schwung im Hangverhalten mit ½ Dr.",
+            "4 Schwünge ODER Spreizumschwung (Mühle)",
+            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
+            "Kipphang 2 Sek. ODER Vorschwingen zum Kästchen 'Beliebige Kippe'",
+            "Beliebige Kippe",
+            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
+            "Hüftumschwung vorlings rückwärts",
+            "VERBINDUNG Umschwung - Unterschwung",
+            "Beliebiger Unterschwung"
+    };
+
+    public static final String[] Stufenbarren = {
+            "Hüftaufschwung ODER Schwungaufschwung ODER 2. Kippe",
+            "Rückschwung zum Aufhocken/-bücken und Sprung zum Hang am o.H.",
+            "4 Schwünge ODER Spreizumschwung (Mühle)",
+            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
+            "Kipphang 2 Sek. ODER Vorschweben-Rückschweben ODER Vorschweben/-schwingen zum Kästchen 'Beliebige Kippe'",
+            "Beliebige Kippe",
+            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
+            "Hüftumschwung vorlings rückwärts",
+            "VERBINDUNG Umschwung - Unterschwung",
+            "Beliebiger Unterschwung"
+    };
+
+    public static final String[] Balken = {
+            "Beliebiger Aufgang",
+            "Arabeske 2 Sek. ODER ½ Drehung auf beiden Beinen",
+            "Beliebige Standwaage 2 Sek.",
+            "½ Drehung vw. auf einem Bein",
+            "Beidbeiniger Sprung (ohne LAD und Querspreizwinkel) auch als Abgang",
+            "Pferdchensprung ODER Schersprung",
+            "Sprung mit Querspreizwinkel 90° (alle Varianten)",
+            "Handstand",
+            "Rad ODER beliebige Rolle vw./rw.",
+            "Abgang: Rondat ODER Überschlag vw. ODER Salto vw./rw."
+    };
+
+    public static final String[] Pferd = {
+            "Sprung in den Seitsütz vl.",
+            "Gewichtsverlagerung links und rechts im Seitsütz vl.",
+            "Seitschwingen mit Spreizen links und rechts im Seitsütz vl.",
+            "Seitschwingen mit Vorspreizen",
+            "Seitschwingen mit Rückspreizen",
+            "VERBINDUNG von Seitschwingen mit Vorspreizen und Seitschwingen mit Rückspreizen",
+            "VERBINDUNG auf der anderen Seite: Seitschwingen mit Vorspreizen und Seitschwingen mit Rückspreizen",
+            "Kreisspreizen eines Beines",
+            "Schere vw.",
+            "Abgang: Kehre ODER Flanke ODER Wende"
+    };
+}
--- app/src/main/java/com/lerchenflo/t10elementekatalog/punktezaehlerActivity.java
+++ app/src/main/java/com/lerchenflo/t10elementekatalog/punktezaehlerActivity.java
@@ -1,5 +1,14 @@
 package com.lerchenflo.t10elementekatalog;
 
+import static com.lerchenflo.t10elementekatalog.constants.Balken;
+import static com.lerchenflo.t10elementekatalog.constants.Barren;
+import static com.lerchenflo.t10elementekatalog.constants.Boden;
+import static com.lerchenflo.t10elementekatalog.constants.Hochreck;
+import static com.lerchenflo.t10elementekatalog.constants.Pferd;
+import static com.lerchenflo.t10elementekatalog.constants.Ringe;
+import static com.lerchenflo.t10elementekatalog.constants.Stufenbarren;
+import static com.lerchenflo.t10elementekatalog.constants.Tiefreck;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -19,113 +28,6 @@ public class punktezaehlerActivity extends AppCompatActivity {
 
     private Button[] buttons = new Button[10];
     // New arrays with "ODER" options
-
-    private String[] Boden = {
-            "Beliebige Rolle vorwärts",
-            "Beliebige Rolle rückwärts ODER Kerze 2 Sek.",
-            "Krafthandstand ODER Sprung mit Querspreizwinkel 135°",
-            "C+Position in Rückenlage 2 Sek. ODER Kopfstand 2 Sek ODER 1/1 Dr. vw. auf gestreckem Bein",
-            "VERBINDUNG Chassé – Pferdchen-/Schersprung ODER Sprung mit 1/1 Dr.",
-            "Beliebige Standwaage 2 Sek. ODER Spagat 2 Sek. ODER Winkelstütz 2 Sek",
-            "Handstand ODER Handstand mit mind. ½ Dr.",
-            "Rad ODER einarmiges Rad",
-            "Rondat (Radwende)",
-            "Handstützüberschlag vw. ODER Handstützüberschlag rw. (Flick-Flack)",
-            "Salto vorwärts geh. ODER Salto rückwärts geh."
-    };
-
-    private String[] Barren = {
-            "Sprung in den Stütz und Vorschwung ODER beliebige (andere) Schwungstemme",
-            "4 Schwünge",
-            "Vorschwung in den Grätschsitz ODER Oberarmkippe in den Grätschsitz",
-            "Außenquersitz",
-            "Winkelstütz 2 Sek.",
-            "Rolle vw. ODER Rolle rw.",
-            "Oberarmstand 2 Sek.",
-            "Beliebige Kippe in den Stütz",
-            "Beliebige (andere) Schwungstemme ODER Rückschwung in den Handstand",
-            "Abgang: Kehre ODER Wende ODER Kreishocke"
-    };
-
-    private String[] Ringe = {
-            "Stütz 2 Sek.",
-            "Winkelstütz 2 Sek. ODER Grätschwinkelstütz 2 Sek.",
-            "Kontrolliertes Absenken ODER Abrollen aus dem Stütz",
-            "Beugehang 2 Sek.",
-            "Winkelhang 2 Sek. mit gehockten ODER gestreckten Beinen",
-            "Strecksturzhang 2 Sek.",
-            "Aus (Streck-)Sturzhang senken zum Hang(stand) rl.",
-            "Heben vom Hang rl. zum Sturzhang",
-            "4 Schwünge",
-            "Abgang: Salto vorwärts ODER Salto rückwärts"
-    };
-
-
-
-    private String[] Tiefreck = {
-            "Hüftaufschwung",
-            "Sprung in den Stütz vl. ODER Vor- oder Rückspreizen eines Beines",
-            "Rück- und Vordrehen im Hang ODER Spreizumschwung (Mühle) ODER Sitzumschwung",
-            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
-            "Kipphang 2 Sek. ODER Vorschweben-Rückschweben ODER Vorschweben zum Kästchen 'Beliebige Kippe'",
-            "Beliebige Kippe",
-            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
-            "Hüftumschwung vorlings rückwärts",
-            "VERBINDUNG Umschwung - Unterschwung",
-            "Abgang: Beliebiger Unterschwung ODER Hocke/Hockwende"
-    };
-
-    private String[] Hochreck = {
-            "Hüftaufschwung ODER Schwungaufschwung ODER 2. Kippe ODER Schwungstemme rw.",
-            "Vor- oder Rückspreizen eines Beines ODER Schwung im Hangverhalten mit ½ Dr.",
-            "4 Schwünge ODER Spreizumschwung (Mühle)",
-            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
-            "Kipphang 2 Sek. ODER Vorschwingen zum Kästchen 'Beliebige Kippe'",
-            "Beliebige Kippe",
-            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
-            "Hüftumschwung vorlings rückwärts",
-            "VERBINDUNG Umschwung - Unterschwung",
-            "Beliebiger Unterschwung"
-    };
-
-    private String[] Stufenbarren = {
-            "Hüftaufschwung ODER Schwungaufschwung ODER 2. Kippe",
-            "Rückschwung zum Aufhocken/-bücken und Sprung zum Hang am o.H.",
-            "4 Schwünge ODER Spreizumschwung (Mühle)",
-            "Hüftumschwung vorlings vorwärts ODER Hüftabschwung vl. vw.",
-            "Kipphang 2 Sek. ODER Vorschweben-Rückschweben ODER Vorschweben/-schwingen zum Kästchen 'Beliebige Kippe'",
-            "Beliebige Kippe",
-            "Rückschwung aus dem Stütz (auch zum Niedersprung)",
-            "Hüftumschwung vorlings rückwärts",
-            "VERBINDUNG Umschwung - Unterschwung",
-            "Beliebiger Unterschwung"
-    };
-
-    private String[] Balken = {
-            "Beliebiger Aufgang",
-            "Arabeske 2 Sek. ODER ½ Drehung auf beiden Beinen",
-            "Beliebige Standwaage 2 Sek.",
-            "½ Drehung vw. auf einem Bein",
-            "Beidbeiniger Sprung (ohne LAD und Querspreizwinkel) auch als Abgang",
-            "Pferdchensprung ODER Schersprung",
-            "Sprung mit Querspreizwinkel 90° (alle Varianten)",
-            "Handstand",
-            "Rad ODER beliebige Rolle vw./rw.",
-            "Abgang: Rondat ODER Überschlag vw. ODER Salto vw./rw."
-    };
-
-    private String[] Pferd = {
-            "Sprung in den Seitsütz vl.",
-            "Gewichtsverlagerung links und rechts im Seitsütz vl.",
-            "Seitschwingen mit Spreizen links und rechts im Seitsütz vl.",
-            "Seitschwingen mit Vorspreizen",
-            "Seitschwingen mit Rückspreizen",
-            "VERBINDUNG von Seitschwingen mit Vorspreizen und Seitschwingen mit Rückspreizen",
-            "VERBINDUNG auf der anderen Seite: Seitschwingen mit Vorspreizen und Seitschwingen mit Rückspreizen",
-            "Kreisspreizen eines Beines",
-            "Schere vw.",
-            "Abgang: Kehre ODER Flanke ODER Wende"
-    };
 
 
 
